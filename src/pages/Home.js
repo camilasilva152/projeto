7fd2cc8e7c97e8fa625fc7980fd3eb461e343f75
Gabriel Pinheiro <gabriel.pinheiro.dev@outlook.com>
2022-12-03 18:26:21 -0300

    adiciona icones e mostra os dados na tela

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,46 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import './Home.css';
-import trash from '../assets/trash.png';
-import edit from '../assets/edit.png';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Home.css";
+import trash from "../assets/trash.png";
+import edit from "../assets/edit.png";
 
 function Home() {
-  const [user, setUser] = useState();
+  const [users, setUsers] = useState();
 
   useEffect(() => {
-  axios.get('https://randomuser.me/api/?results=5').then(response => {
-    setUser(response.data.results);
-  });
+    axios.get("https://randomuser.me/api/?results=5").then((response) => {
+      setUsers(response.data.results);
+    });
   }, []);
-  
-  useEffect(() => {
-  console.log(user);
-  }, [user]);
-    return (
-        <div className="wrapper-home">
-          <div className="container-home">
-            <div className='header'>
-              <h1>Lista de pacientes</h1>
-            </div>
-            <div className='item'>
-              <span>Nome</span>
-              <span>Gênero</span>
-              <span>Aniversário</span>
-              <span>Ações</span>
-            {user.map(user => {
-            return (
-            <div key={user.email}>
-            <p>{user.gender}</p>
-      </div>
-    )
-  })}
-            </div>
-          </div>
+
+  return (
+    <div className="wrapper-home">
+      <div className="container-home">
+        <div className="header">
+          <h1>Lista de pacientes</h1>
         </div>
-        
-    );
-  }
-  export default Home;
+        <div className="item">
+          <span>Nome</span>
+          <span>Gênero</span>
+          <span>Aniversário</span>
+          <span>Ações</span>
+        </div>
+        {users &&
+          users.map((user) => (
+            <div className="item">
+              <span>
+                {user.name.first} {user.name.last}
+              </span>
+              <span>{user.gender}</span>
+              <span>{new Date(user.dob.date).toLocaleDateString()}</span>
+              <span>
+                <img src={trash} alt="icone deletar" />
+                <img src={edit} alt="icone editar" />
+              </span>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+export default Home;
